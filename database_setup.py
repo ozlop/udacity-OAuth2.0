32417d31a5353d917b8814b1193c8def1aaaf61c
ozlop <oswaldo.t.lopez@gmail.com>
2018-12-16 11:51:09 -0800

    Load new database with sample data

--- database_setup.py
+++ database_setup.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
@@ -57,7 +57,7 @@ class MenuItem(Base):
        }
 
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
 
 
 Base.metadata.create_all(engine)
